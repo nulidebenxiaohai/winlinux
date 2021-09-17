a3ca7d6ef45556f350ba08a112fd369c7973cb0d
nulidebenxiaohai <2033575040@qq.com>
2021-09-18 00:35:13 +0100

    9月18日

---
+++ birmingham/Python/greeter.py
@@ -0,0 +1,12 @@
+#input()函数编写清清楚楚的程序
+name = input("Please enter your name: ")
+print(f"\nHello, {name}!")
+
+#多行的输入
+prompt = "If you tell use who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name?"
+
+name = input(prompt)
+print(f"\nHellp, {name}!")  
+
+#
