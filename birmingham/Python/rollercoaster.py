c84a30c5ffd5aa1daafe9b2ea05255be56300376
nulidebenxiaohai <2033575040@qq.com>
2021-09-19 15:12:04 +0100

    9月19日

---
+++ birmingham/Python/rollercoaster.py
@@ -0,0 +1,8 @@
+#实际函数中使用函数int()
+height = input("How tall are you, in inches? ")
+height = int(height)
+
+if height >= 48:
+    print("\nYou're tall enough to ride!")
+else:
+    print("\nYou'll be able to ride when you're a little older.")
