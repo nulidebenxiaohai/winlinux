07ff64457b00553740937828a0ff49e384a8f32d
nulidebenxiaohai <2033575040@qq.com>
2021-09-22 23:12:11 +0100

    note update

--- birmingham/Python/parrot.py
+++ birmingham/Python/parrot.py
@@ -2,8 +2,12 @@
 prompt = "\nTell me something, and I will repeat it back to you: "
 prompt += "\nEnter 'quit' to end the program. "
 message = ""
-while message != 'quit':
+active = True
+
+while active:
     message = input(prompt)
 
-    if message != 'quit':
+    if message == 'quit':
+        active = False
+    else:
         print(message)
