3ffaf385eb08296322381e4d2e8740675458025a
mac air <2033575040@qq.com>
2021-09-26 00:14:22 +0100

    Dog类

---
+++ birmingham/Python/dog.py
@@ -0,0 +1,16 @@
+#创建Dog类
+class Dog:
+	"""一次模拟小狗的简单尝试"""
+	
+	def _init_(self, name, age):
+		"""初始化属性name 和 age"""
+		self.name = name
+		self.age = age
+
+	def sit(self):
+		"""模拟小狗收到命令时蹲下。"""
+		print(f"{self.name} is now sitting.")
+	
+	def roll_over(self):
+		"""模拟小狗收到命令时打滚。"""
+		print(f"{self.name} rolled over!")
