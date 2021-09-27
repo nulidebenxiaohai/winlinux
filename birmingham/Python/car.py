7cffc52bc5c6e56629f90e6089325774553aee78
nulidebenxiaohai <2033575040@qq.com>
2021-09-27 23:47:16 +0100

    9月27日在类中给属性指定默认值

--- birmingham/Python/car.py
+++ birmingham/Python/car.py
@@ -7,11 +7,17 @@ class Car(object):
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """返回整洁的描述性信息"""
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
 
+    def read_odometer(self):
+        """打印一条指出汽车里程的消息"""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
 my_new_car = Car('audi','a4',2019)
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
