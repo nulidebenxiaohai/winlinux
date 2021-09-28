b2b45304a1deb5ac45077ac9249b13fd112a1e4f
nulidebenxiaohai <2033575040@qq.com>
2021-09-28 23:05:22 +0100

    子类的__init__方法

---
+++ birmingham/Python/electric_car.py
@@ -0,0 +1,39 @@
+#表示Car的类
+class Car(object):
+    """一次模拟汽车的简单尝试"""
+
+    def __init__(self, make, model, year):
+        """初始化描述汽车的属性"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """返回整洁的描述性信息"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """打印一条指出汽车里程的消息"""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odomenter(self, miles):
+        self.odometer_reading += miles
+
+class ElectricCar(Car):
+    """电动汽车的独特之处"""
+
+    def __init_(self, make, model, year):
+        """初始化父类的属性(super函数是一个特殊函数让你能调用父类的方法）"""
+        super().__init__(make, model, year)
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+
