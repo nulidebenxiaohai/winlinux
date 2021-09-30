41af5725002e657b6172965f733b2602e223c2fb
nulidebenxiaohai <2033575040@qq.com>
2021-09-30 23:36:32 +0100

    游戏项目绘制飞船

--- birmingham/Python/alien_invasion/alien_invasion.py
+++ birmingham/Python/alien_invasion/alien_invasion.py
@@ -2,15 +2,23 @@ import sys
 
 import pygame
 
+from settings import Settings
+
+from ship import Ship
 class AlienInvasion:
     """管理游戏资源和行为的类"""
 
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
+        self.settings = Settings() 
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)
+        # 设置背景色 RGB原理
+        self.bg_color = self.settings.bg_color
 
     def run_game(self):
         """开始游戏的主旋律"""
@@ -19,10 +27,13 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            
+            # 每次循环时都会重绘屏幕
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             # 让最近绘制的屏幕可见
             pygame.display.flip()
-
+    
 if __name__ == '__main__':
     # 创造游戏实例并且运行游戏
     ai = AlienInvasion()
