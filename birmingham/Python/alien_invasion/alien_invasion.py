b05183a0ade8ab0ff7fd8aaca7bd7dd25deb24cd
nulidebenxiaohai <2033575040@qq.com>
2021-10-02 14:30:47 +0100

    游戏项目绘制飞船重构方法

--- birmingham/Python/alien_invasion/alien_invasion.py
+++ birmingham/Python/alien_invasion/alien_invasion.py
@@ -24,15 +24,24 @@ class AlienInvasion:
         """开始游戏的主旋律"""
         while True:
             # 监视键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
+            self._check_event()
+
             # 每次循环时都会重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+            self._update_screen()
+
+    def _check_event(self):
+        """相应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """更新屏幕上的图像，并且切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
     
 if __name__ == '__main__':
     # 创造游戏实例并且运行游戏
