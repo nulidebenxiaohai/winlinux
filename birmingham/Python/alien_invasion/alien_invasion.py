2f38764cffb57e803b8744acc5005c8ba9005ae8
mac air <2033575040@qq.com>
2021-10-03 23:32:17 +0100

    mac缩进问题

--- birmingham/Python/alien_invasion/alien_invasion.py
+++ birmingham/Python/alien_invasion/alien_invasion.py
@@ -46,8 +46,11 @@ class AlienInvasion:
         if event.key == pygame.K_RIGHT:
             #向右移动飞船
             self.ship.moving_right = True
-        elif event.key ==pygame.K_LEFT:
+	    elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+		elif event.key ==pygame.K_q:
+		    sys.exit()
+
 
     def _check_keyup_events(self, event):
         """响应松开"""
