03e80b4ea26d53d2a3dc6140d5fd0db1c5bf814c
nulidebenxiaohai <2033575040@qq.com>
2021-10-08 01:08:14 +0100

    switch语句具有穿透性

---
+++ birmingham/Java/SwitchNotice.java
@@ -0,0 +1,19 @@
+public class SwitchNotice {
+    public static void main(String[] args){
+        int num = 2;
+		switch (num) {
+            case 1:
+				System.out.println("nihao");
+				break;
+			case 2:
+				System.out.println("wohao");
+				//break
+			case 3:
+				System.out.println("dajiahao");
+				break;
+			default:
+				System.out.println("tahao,woyehaoao");
+				break;
+		}
+	}
+}
