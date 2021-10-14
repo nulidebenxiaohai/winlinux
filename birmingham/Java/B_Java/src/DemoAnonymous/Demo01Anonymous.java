c5a9a15b4dadad83ac597657005f33ff3ee23d6b
mac air <2033575040@qq.com>
2021-10-14 16:51:39 +0100

    Java 匿名对象的说明

--- birmingham/Java/B_Java/src/DemoAnonymous/Demo01Anonymous.java
+++ birmingham/Java/B_Java/src/DemoAnonymous/Demo01Anonymous.java
@@ -5,6 +5,9 @@ package DemoAnonymous;
 
 匿名对象就是只有右边的对象，没有左边的名字和赋值运算符。
 new 类名称（）；
+
+注意事项：匿名对象只能使用唯一的一次。下次再用不得不再创建一个新对象
+使用建议：若确定只使用对象一次，就可以使用匿名对象。
  */
 public class Demo01Anonymous {
     public static void main(String[] args) {
