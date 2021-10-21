5c7b394eac9a1c4900a4e8e68c43117451cc39dc
mac air <2033575040@qq.com>
2021-10-21 21:10:31 +0100

    继承的概念

---
+++ birmingham/Java/B_Java/src/Day09/Demo01/Employee.java
@@ -0,0 +1,7 @@
+package Day09.Demo01;
+//定义一个父类：员工
+public class Employee {
+    public void method(){
+        System.out.println("Method execute");
+    }
+}
---
+++ birmingham/Java/B_Java/src/Day09/Demo01/Teacher.java
@@ -0,0 +1,5 @@
+package Day09.Demo01;
+
+public class Teacher extends Employee{
+    //Teacher 类中虽然什么都没有写，但是会继承来自父类的method方法
+}
