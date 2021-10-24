dedcecae721ea9c06113202099a58c8094140b13
mac air <2033575040@qq.com>
2021-10-24 16:32:31 +0100

    abstract method defination and use

---
+++ birmingham/Java/B_Java/src/Day09/Demo11/Cat.java
@@ -0,0 +1,8 @@
+package Day09.Demo11;
+
+public class Cat extends Animal{
+    @Override
+    public void eat(){
+        System.out.println("Cat eat fish");
+    }
+}
---
+++ birmingham/Java/B_Java/src/Day09/Demo11/DemoMain.java
@@ -0,0 +1,8 @@
+package Day09.Demo11;
+
+public class DemoMain {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.eat();
+    }
+}
