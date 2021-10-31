ed49e01e75da1a87cb4eaa71fc2379ec77387058
yujiahua86@gmail.com <38655907+nulidebenxiaohai@users.noreply.github.com>
2021-10-31 22:51:56 +0000

    四种权限修饰符

--- birmingham/Java/B_Java/src/Day11/Demo02/Demo01Main.java
+++ birmingham/Java/B_Java/src/Day11/Demo02/Demo01Main.java
@@ -1,6 +1,11 @@
 package Day11.Demo02;
 /*
-
+Java中有四种权限修饰符：
+             public > protected > (default) > private
+同一个类        Yes         Yes        Yes         Yes
+同一个包        Yes         Yes        Yes         No
+不同包子类      Yes          Yes        No         No
+不同包非子类    Yes          No        No         No
  */
 public class Demo01Main {
 }
