561277412b9781eff7a3e19e13b0c0e5068df9e7
yujiahua86@gmail.com <38655907+nulidebenxiaohai@users.noreply.github.com>
2021-11-04 23:43:01 +0000

    局部内部类的final问题

--- birmingham/Java/B_Java/src/Day11/Demo04/MyOuter.java
+++ birmingham/Java/B_Java/src/Day11/Demo04/MyOuter.java
@@ -5,7 +5,10 @@ package Day11.Demo04;
 备注：从Java 8+开始，只要局部变量事实不变，那么final关键字可以省略
 
 原因：
-1.
+1.new出来的对象在堆内存当中
+2. 局部变量是跟着方法走的，在栈内存中
+3. 方法运行结束之后，立刻出栈
+4. 但是new出来的对象会在堆当中存在，直到垃圾回收消失
  */
 public class MyOuter {
     public void methodOuter() {
