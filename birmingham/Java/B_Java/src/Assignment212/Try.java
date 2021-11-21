fa40ed5e8e36f43ce67262ec654ddbc6bfd49d7f
mac air <2033575040@qq.com>
2021-11-21 21:38:51 +0000

    dating

--- birmingham/Java/B_Java/src/Assignment212/Try.java
+++ birmingham/Java/B_Java/src/Assignment212/Try.java
@@ -6,7 +6,6 @@ public class Try {
         sb.append("enter");
         sb.append("\n");
         sb.append("01");
-<<<<<<< HEAD
         String abs = "enter\n01";
         System.out.println(sb);
         switch (abs){
@@ -20,8 +19,8 @@ public class Try {
                 System.out.println("right");
                 break;
         }
-=======
+
         System.out.println(sb);//
->>>>>>> 787fb13f9bbe5c2078b0a667a601ba2055cc1ac4
+
     }
 }
