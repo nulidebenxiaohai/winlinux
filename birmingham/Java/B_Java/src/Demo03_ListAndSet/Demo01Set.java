719e97301ed1f3edb38448c2ee00e53e20a3f97e
yujiahua86@gmail.com <38655907+nulidebenxiaohai@users.noreply.github.com>
2021-11-27 00:12:12 +0000

    Set集合

---
+++ birmingham/Java/B_Java/src/Demo03_ListAndSet/Demo01Set.java
@@ -0,0 +1,41 @@
+package Demo03_ListAndSet;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/*
+java.util.Set接口 extend Collection接口
+Set接口的特点：
+    1.不允许存储重复的元素
+    2.没有索引，没有带索引的方法，也不能使用普通的for循环遍历
+java.util.HashSet集合 implement Set接口
+Hash Set特点：
+    1.不允许存储重复的元素
+    2.没有索引，没有带索引的方法，也不能使用普通的for循环遍历
+    3.是一个无序的集合，存储元素和取出元素的顺序可能不一致
+    4.底层是一个哈希表结构（查询的速度非常的快）
+ */
+public class Demo01Set {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        //使用add方法往集合中添加元素
+        set.add(1);
+        set.add(2);
+        set.add(4);
+        set.add(3);
+        set.add(1);
+        //使用迭代遍历set集合
+        Iterator<Integer> it = set.iterator();
+        while(it.hasNext()){
+            Integer n = it.next();
+            System.out.println(n);
+        }
+        //使用增强for遍历set集合
+        System.out.println("------------------");
+        for (Integer i : set) {
+            System.out.println(i);
+        }
+    }
+}
