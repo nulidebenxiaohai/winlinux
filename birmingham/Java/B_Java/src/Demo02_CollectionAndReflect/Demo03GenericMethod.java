b1a82c87918e88d45f0a13c205fded069517ba07
mac air <2033575040@qq.com>
2021-11-28 21:11:02 +0000

    具有泛型的方法

---
+++ birmingham/Java/B_Java/src/Demo02_CollectionAndReflect/Demo03GenericMethod.java
@@ -0,0 +1,12 @@
+package Demo02_CollectionAndReflect;
+/*
+测试含有泛型的方法
+ */
+public class Demo03GenericMethod {
+    public static void main(String[] args) {
+        //创建GenericMethod对象
+        GenericMethod gm = new GenericMethod();
+        gm.method01(1);
+        gm.method01("nihaowoshiyujiahua");
+    }
+}
