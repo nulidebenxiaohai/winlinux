0ad6d5def6b85db689769f798f24296e3555ab47
yujiahua86@gmail.com <38655907+nulidebenxiaohai@users.noreply.github.com>
2021-11-28 00:57:26 +0000

    Iterator

---
+++ birmingham/Java/B_Java/src/Demo02_CollectionAndReflect/Demo01Iterator.java
@@ -0,0 +1,10 @@
+package Demo02_CollectionAndReflect;
+/*
+java.util.Iterator接口：迭代器（对集合进行遍历）
+有两个常用的方法：
+    boolean hasNext()如果仍有元素可以迭代，则返回true
+        判读集合中还有没有下一个元素，有就返回true，没有就返回false
+    E next()返回迭代的下一个元素
+Iterator迭代器，是一个接口，我们无法直接使用，需要使用Iterator接口的实现对象，获取实现列的方式比较特殊 */
+public class Demo01Iterator {
+}
