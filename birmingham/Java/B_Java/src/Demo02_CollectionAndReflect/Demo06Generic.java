7950c04ffb9597794474dfd1ea55361070636434
yujiahua86@gmail.com <38655907+nulidebenxiaohai@users.noreply.github.com>
2021-11-29 15:10:56 +0000

    泛型的上限和下限，没懂

---
+++ birmingham/Java/B_Java/src/Demo02_CollectionAndReflect/Demo06Generic.java
@@ -0,0 +1,10 @@
+package Demo02_CollectionAndReflect;
+/*
+泛型的上限限定：？ extend E   代表使用的泛型只能是E类的子类/本身
+泛型的下限限定：？ super E    代表使用的泛型只能是E类的父类/本身
+ */
+public class Demo06Generic {
+    public static void main(String[] args) {
+
+    }
+}
