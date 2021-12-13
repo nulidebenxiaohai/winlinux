2d6e91386588a16307684d8bfb546a625bbb964b
mac air <2033575040@qq.com>
2021-12-13 23:02:43 +0000

    DS_Store

---
+++ birmingham/Java/B_Java/src/Assignment213/Try.java
@@ -0,0 +1,9 @@
+package Assignment213;
+
+public class Try {
+    public static void main(String[] args) {
+        String str = new String();
+        str = "aabbccddeeffgghh";
+        System.out.println(str.indexOf("dc"));
+    }
+}
