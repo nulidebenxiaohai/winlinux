229cd7a295b2a5c7460d97550ca1014d61de8065
mac air <2033575040@qq.com>
2022-03-26 17:02:35 +0000

    codeforces71A

--- birmingham/CodeForce/src/P71A.java
+++ birmingham/CodeForce/src/P71A.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class P71A {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        //nextInt()只会读取数值，会剩下一个\n;
+        int number = in.nextInt();
+        //nextLine()会读取"\n"，并结束;
+        in.nextLine();
+        while(number > 0){
+            String str = in.nextLine();
+            if (str.length() <= 10){
+                System.out.println(str);
+            }
+            else{
+                String mid = str.length()-2 + "";
+                System.out.println(str.charAt(0) + mid + str.charAt(str.length()-1));
+            }
+            number--;
+        }
     }
 }
